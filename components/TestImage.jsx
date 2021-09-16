954fe0670a975b9163c7261894d53f20164aaba7
Shafee <shafeelinks@gmail.com>
2021-09-15 22:53:39 -0400

    base added with google auth to start working on app

---
+++ components/TestImage.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+function TestImage() {
+  const [data, setData] = useState([]);
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default TestImage;
